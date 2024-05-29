7a4d0abcce056fb7371096b5926166ce05a4a615
miranda <mjjacobo@scl.edu.gt>
2024-05-29 11:45:11 -0600

    actualizo-header

---
+++ foorte/footer.js
@@ -0,0 +1,6 @@
+let genn_footer = document.querySelector(".footer")
+
+genn_footer.innerHTML = `
+   <h1 class="titulo">scl.edu.gt</h1>
+   
+`
---
+++ header/header.js
@@ -0,0 +1,6 @@
+let genn_header = document.querySelector(".header")
+
+genn_header.innerHTML = `
+   <h1 class="titulo">Comandos Git</h1>
+   <a href class="btn-Github">Github</a>
+`
